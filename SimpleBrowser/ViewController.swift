8ad001528f3f9d0ece734363f533860bb7144542
shimpeiws <shimpeiws@gmail.com>
2016-04-30 16:45:51 +0900

    Load initial web page to web view

--- SimpleBrowser/ViewController.swift
+++ SimpleBrowser/ViewController.swift
@@ -14,15 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var backButton: UIBarButtonItem!
     @IBOutlet weak var reloadButton: UIBarButtonItem!
     @IBOutlet weak var stopButton: UIBarButtonItem!
+    
+    let homeUrl = "https://www.google.co.jp"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        openUrl(homeUrl)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func openUrl(urlString: String) {
+        let url = NSURL(string: urlString)
+        let urlRequest = NSURLRequest(URL: url!)
+        webView.loadRequest(urlRequest)
     }
     
     @IBAction func backButtonTapped(sender: UIBarButtonItem) {
