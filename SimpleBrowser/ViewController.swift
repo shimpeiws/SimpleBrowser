5b20d884d46d8ee99530971c6649c3a3bcc52c6f
shimpeiws <shimpeiws@gmail.com>
2016-04-30 16:37:45 +0900

    Add outlets and actions

--- SimpleBrowser/ViewController.swift
+++ SimpleBrowser/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    @IBOutlet weak var stopButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func backButtonTapped(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func reloadButtonTapped(sender: UIBarButtonItem) {
+    }
+    
+    
+    @IBAction func stopButtonTapped(sender: UIBarButtonItem) {
+    }
 
 }
 
