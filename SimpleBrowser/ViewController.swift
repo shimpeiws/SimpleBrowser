0f19bd9c33760057ae215dd246cf8fbf4081de21
shimpeiws <shimpeiws@gmail.com>
2016-05-01 23:11:41 +0900

    Add webView delegated actions

--- SimpleBrowser/ViewController.swift
+++ SimpleBrowser/ViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var backButton: UIBarButtonItem!
     @IBOutlet weak var reloadButton: UIBarButtonItem!
     @IBOutlet weak var stopButton: UIBarButtonItem!
-    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
     let homeUrl = "https://www.google.co.jp"
+    
+    let whiteList = [
+        "https?://*\\.google\\.co\\.jp",
+        "https?://*\\.google\\.com",
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +39,64 @@ class ViewController: UIViewController {
         webView.loadRequest(urlRequest)
     }
     
+    func openUrlInSafari(urlString: String) {
+        if let nsUrl = NSURL(string: urlString) {
+            UIApplication.sharedApplication().openURL(nsUrl)
+        }
+    }
+    
+    func stopLoading() {
+        activityIndicator.alpha = 0
+        activityIndicator.stopAnimating()
+        backButton.enabled = webView.canGoBack
+        reloadButton.enabled = true
+        stopButton.enabled = false
+    }
+    
+    // MARK: - UIWebViewDelegate
+    func webViewDidStartLoad(webView: UIWebView) {
+        activityIndicator.alpha = 1
+        activityIndicator.startAnimating()
+        backButton.enabled = false
+        reloadButton.enabled = false
+        stopButton.enabled = true
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        stopLoading()
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if navigationType == UIWebViewNavigationType.Other {
+            return true;
+        }
+        
+        var theUrl: String
+        if let unwrappedUrl = request.URL?.absoluteString {
+            theUrl = unwrappedUrl
+        } else {
+            stopLoading()
+            return false;
+        }
+        
+        var canStayInApp = false;
+        for url in whiteList {
+            if let match = theUrl.rangeOfString(url, options: NSStringCompareOptions.RegularExpressionSearch) {
+                canStayInApp = true;
+                break;
+            }
+        }
+        
+        if !canStayInApp {
+            openUrlInSafari(theUrl)
+            stopLoading()
+            return false;
+        }
+        
+        return true
+    }
+    
+    // MARK: - IBAction
     @IBAction func backButtonTapped(sender: UIBarButtonItem) {
         webView.goBack()
     }
